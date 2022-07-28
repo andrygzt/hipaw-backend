9bc5c259b5070f8e98ad3df333809587f20ba3b3
Andrea Garcia <andrygzt@gmail.com>
2022-07-28 00:42:45 -0700

    routes completed

--- app/models/pet.py
+++ app/models/pet.py
@@ -4,6 +4,7 @@ class Pet(db.Model):
     pet_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     pet_name = db.Column(db.String)
     detail = db.Column(db.String)
+    photo = db.Column(db.bytea)
     posts= db.relationship('Post', back_populates='pet', lazy=True)
     user_id = db.Colummn(db.Integer, db.ForeingKey('user.user_id'), nullable=False)
     user = db.relationship('User', back_populates='user', lazy=True)
--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,7 @@ class User(db.Model):
     user_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String)
     location = db.Column(db.String)
-    posts= db.relationship('Post', back_populates='users', lazy=True)
+    posts= db.relationship('Post', back_populates='user', lazy=True)
     pets= db.relationship('Pet', back_populates='user', lazy=True)
 
 def to_dict(self):
